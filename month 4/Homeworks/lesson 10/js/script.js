de801202291ff316d7bc758e69671c464351ab61
hasanjon <hasanjon1224@gmail.com>
2024-11-19 10:21:37 -0600

    first commit

---
+++ month 4/Homeworks/lesson 10/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", function () {
+  let products = document.querySelectorAll(".product"),
+    buttons = document.querySelectorAll("button"),
+    openBtn = document.querySelector(".open");
+
+  function createCart() {
+    let cart = document.createElement("div");
+    let field = document.createElement("div");
+    let heading = document.createElement("h2");
+    let closeBtn = document.createElement("button");
+    
+  }
+});
